0c7f3f1108278a29c8e06a3acccaaa9cff1cfd7f
iKO <TinXery@gmail.com>
2025-04-13 03:46:54 +0200

    Test

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,7 @@
+﻿Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine("" + roll);
+
+Random diceTwo = new();
+int rollTwo = diceTwo.Next(1,21);
+Console.WriteLine("" + rollTwo);
