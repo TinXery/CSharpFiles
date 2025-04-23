52adf36487a7475d74cbde646b127b0515d5b3fa
iKO <TinXery@gmail.com>
2025-04-24 01:45:50 +0200

    nothing really

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,7 +1,8 @@
-﻿Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine("" + roll);
+﻿Random dice = new();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(05, 101);
 
-Random diceTwo = new();
-int rollTwo = diceTwo.Next(1,21);
-Console.WriteLine("" + rollTwo);
+Console.WriteLine("" + roll1);
+Console.WriteLine("" + roll2);
+Console.WriteLine("" + roll3);
